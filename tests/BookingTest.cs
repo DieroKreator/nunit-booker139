6d36d1da18b8356a01b172a5a79bf4ffb7ba4cf8
DieroKreator <qtechwired@gmail.com>
2025-03-25 18:09:44 -0300

    first commit

---
+++ tests/BookingTest.cs
@@ -0,0 +1,12 @@
+namespace Booking;
+
+public class BookingTest
+{
+    private const string BASE_URL = "https://restful-booker.herokuapp.com/booking";
+
+    [Test, Order(1)]
+    public void PostBookingTest()
+    {
+        
+    }
+}
