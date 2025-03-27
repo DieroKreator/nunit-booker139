ac14f742839e7f94db77363efcfb7e220406b5fd
DieroKreator <qtechwired@gmail.com>
2025-03-27 16:27:48 -0300

    Add token retrieval method and update booking test to include authorization

--- tests/BookingTest.cs
+++ tests/BookingTest.cs
@@ -6,19 +6,46 @@ namespace Booking;
 public class BookingTest
 {
     private const string BASE_URL = "https://restful-booker.herokuapp.com/";
+    string token = "";
+
+    [Test, Order(0)]
+    public void GetToken()
+    {
+        var client = new RestClient(BASE_URL);
+        var request = new RestRequest("auth", Method.Post);
+
+        request.AddHeader("Content-Type", "application/json");
+
+        var authBody = new
+        {
+            username = "admin", // Default credentials from API docs
+            password = "password123"
+        };
+
+        request.AddJsonBody(authBody);
+
+        var response = client.Execute(request);
+        Console.WriteLine($"Auth Response: {response.Content}");
+
+        var tokenResponse = JsonConvert.DeserializeObject<dynamic>(response.Content);
+        token = tokenResponse.token;
+
+        Console.WriteLine($"Token: {token}");
+    }
 
     [Test, Order(1)]
     public void PostBookingTest()
     {
-        var options = new RestClientOptions(BASE_URL)
-        {
-            Authenticator = new RestSharp.Authenticators.HttpBasicAuthenticator("admin", "password123")
-        };
-
-        var client = new RestClient(BASE_URL);
+        var options = new RestClientOptions("https://restful-booker.herokuapp.com/");
+        var client = new RestClient(options);
         var request = new RestRequest("booking", Method.Post);
 
-        string jsonBody = File.ReadAllText("");
+        // ✅ Manually add the Authorization header
+        request.AddHeader("Content-Type", "application/json");
+        request.AddHeader("Accept", "application/json");
+        request.AddHeader("Authorization", $"Bearer {token}");
+
+        string jsonBody = File.ReadAllText("/Users/dierokreator/Programming/Interasys/nunit-booker139/fixtures/booking1.json");
 
         request.AddBody(jsonBody);
 
