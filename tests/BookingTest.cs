bb44c354d9b45d2547889ff7047be05547042a35
DieroKreator <qtechwired@gmail.com>
2025-03-29 16:03:09 -0300

    Fix booking ID in GetBookingTest and add UpdateBookingTest for modifying booking details

--- tests/BookingTest.cs
+++ tests/BookingTest.cs
@@ -1,3 +1,4 @@
+using Models;
 using Newtonsoft.Json;
 using RestSharp;
 
@@ -69,15 +70,16 @@ public class BookingTest
     }
 
     [Test, Order(2)]
-    public void GetBokkingTest()
+    public void GetBookingTest()
     {
-        int bookingId = 2191;
+        int bookingId = 3694;
         String expFirstName = "Pedro";
         String expLastName = "Perez";
         String expCheckIn = "2025-01-01";
 
         var client = new RestClient(BASE_URL);
         var request = new RestRequest($"booking/{bookingId}", Method.Get);
+        // var request = new RestRequest(Environment.GetEnvironmentVariable("bookingId"), Method.Get);
 
         request.AddHeader("Accept", "application/json");
 
@@ -92,5 +94,46 @@ public class BookingTest
         Assert.That(actualName, Is.EqualTo(expFirstName));
         Assert.That((String)responseBody.lastname, Is.EqualTo(expLastName));
         // Assert.That((String)responseBody.bookingdates[0].expCheckIn, Is.EqualTo(expCheckIn));
+    }
+
+    [Test, Order(3)]
+    public void UpdateBookingTest()
+    {
+        int bookingId = 3694;
+        BookingModel bookingModel = new BookingModel();
+        bookingModel.firstname = "Pedro";
+        bookingModel.lastname = "Perez";
+        bookingModel.totalprice = 500;
+        bookingModel.depositpaid = false;
+        bookingModel.bookingdates = new BookingDates();
+        bookingModel.bookingdates.checkin = "2025-01-01";
+        bookingModel.bookingdates.checkout = "2025-01-02";
+        bookingModel.additionalneeds = "Breakfast";
+
+        var options = new RestClientOptions("https://restful-booker.herokuapp.com/");
+        var client = new RestClient(options);
+        var request = new RestRequest($"booking/{bookingId}", Method.Put);
+
+        request.AddHeader("Content-Type", "application/json");
+        request.AddHeader("Accept", "application/json");
+        request.AddHeader("Cookie", "token=" + token);
+
+        String jsonBody = JsonConvert.SerializeObject(bookingModel, Formatting.Indented);
+        Console.WriteLine(jsonBody);
+        request.AddBody(jsonBody);
+
+        var response = client.Execute(request);
+
+        var responseBody = JsonConvert.DeserializeObject<dynamic>(response.Content);
+        Console.WriteLine(responseBody);
+
+        Assert.That((int)response.StatusCode, Is.EqualTo(200));
+        Assert.That((String)responseBody.firstname, Is.EqualTo("Pedro"));
+        Assert.That((String)responseBody.lastname, Is.EqualTo("Perez"));
+        Assert.That((int)responseBody.totalprice, Is.EqualTo(500));
+        Assert.That((bool)responseBody.depositpaid, Is.EqualTo(false));
+        Assert.That((String)responseBody.bookingdates.checkin, Is.EqualTo("2025-01-01"));
+        Assert.That((String)responseBody.bookingdates.checkout, Is.EqualTo("2025-01-02"));
+        Assert.That((String)responseBody.additionalneeds, Is.EqualTo("Breakfast"));
     }
 }
